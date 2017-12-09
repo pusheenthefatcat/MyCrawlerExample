1c313c11377670d359fc5dc6902c30bd08f564f9
pusheenthefatcat <karmic46@bu.edu>
2017-12-09 15:29:16 +0800

    commit and pushhhhh

--- src/crawler/example/RealPrice.java
+++ src/crawler/example/RealPrice.java
@@ -18,10 +18,8 @@ import org.jsoup.nodes.Element;
 public class RealPrice {
 	public static void main(String[] args) {
 		
-		String uri = "zip:http://plvr.land.moi.gov.tw"
-				+ "/Download?type=zip&fileName=lvr_landxml.zip"
-				+ "!/A_LVR_LAND_A.XML";
-
+		String uri = "zip:http://plvr.land.moi.gov.tw//Download?type=zip&fileName=lvr_landxml.zip!/E_LVR_LAND_A.XML";
+		// http://plvr.land.moi.gov.tw//Download?type=zip&fileName=lvr_landxml.zip
 
 		Document jsoupDoc = CrawlerPack.start()
 				.getFromXml(uri);
